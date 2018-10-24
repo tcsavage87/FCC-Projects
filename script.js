7598ef261f75dbed24ff2e588efc21e8f7a48a01
Taylor Savage <tcsjazz@gmail.com>
2018-10-24 13:19:52 -0500

    Update to js

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-function initBtn () {
-  let button1 = document.getElementById('sous-btn');
-
-  button1.onlick = function() {
-    alert('Hello!!!');
-    button1.innerHTML = 'Selected!';
-    button1.style.background = 'Green';
-  }
+function changeBtn1() {
+  document.getElementById("sous-btn").innerHTML = "Selected!";
+  document.getElementById("sous-btn").style.background = "Green";
 }
 
-function changeBtn2 () {
-  document.getElementById('head-btn').innerHTML = 'Selected!';
-  document.getElementById('head-btn').style.background = 'Green';
+function changeBtn2() {
+  document.getElementById("head-btn").innerHTML = "Selected!";
+  document.getElementById("head-btn").style.background = "Green";
 }
 
-function changeBtn3 () {
-  document.getElementById('exec-btn').innerHTML = 'Selected!';
-  document.getElementById('exec-btn').style.background = 'Green';
+function changeBtn3() {
+  document.getElementById("exec-btn").innerHTML = "Selected!";
+  document.getElementById("exec-btn").style.background = "Green";
 }
 
 function thanks() {
-  alert('Thanks for your interest! We will reach out to you shortly with more details.');
+  alert(
+    "Thanks for your interest! We will reach out to you shortly with more details."
+  );
 }
